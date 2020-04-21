648b4ff125bf5449db6b39cfdf8ee17d3ad92200
na6im <nassim.amokrane@hotmail.com>
2020-04-22 00:14:28 +0100

    Updates

---
+++ precache-manifest.4ff3ca4e37e17e0813dd932aa13acd3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9885bc935b2453ff77dfda77e6a42f3",
+    "url": "/awsome-floating-button/index.html"
+  },
+  {
+    "revision": "1a6e7757817c0e09d378",
+    "url": "/awsome-floating-button/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5a7c253bcffdb921f41b",
+    "url": "/awsome-floating-button/static/js/2.c4ce03ee.chunk.js"
+  },
+  {
+    "revision": "9f55a1ac102148e41f3a9543b31fc30f",
+    "url": "/awsome-floating-button/static/js/2.c4ce03ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6e7757817c0e09d378",
+    "url": "/awsome-floating-button/static/js/main.b241b304.chunk.js"
+  },
+  {
+    "revision": "9384b64a04bdb87e6e04",
+    "url": "/awsome-floating-button/static/js/runtime-main.bd67c5fc.js"
+  }
+]);
